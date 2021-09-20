04c85cbf25b28c4566f8d9f8cf6494bf4297de10
dani <danielabraha99@gmail.com>
2021-09-20 15:51:45 +0100

    code cleanup and extend: include userIO

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ class Character:
         self.date_of_birth: datetime = datetime.datetime.now()
         self.networth: float = 0
         self.family_members = []
+        self.superpowers = None
 
 
-ironman = Character()
-ironman.name = "Tony"
-ironman.age = 36
-ironman.date_of_birth = datetime.date(year=1986, month=3, day=15)
+superman = Character()
+superman.name = "Tony"
+superman.age = 36
+superman.date_of_birth = datetime.date(year=1986, month=3, day=15)
 # print(type(ironman.date_of_birth))
 # print(type(datetime.datetime.now()))
-age = (datetime.datetime.now().date() - ironman.date_of_birth)
+age = (datetime.datetime.now().date() - superman.date_of_birth)
+superman.networth = 20.5
+superman.family_members = [50, "Natasha", "Nick Fury"]
+superman.superpowers = {"flying", "heat vision", "super strength", "super speed", "invincibility"}
+print(f"""
+Superman has the following powers:
+{superman.superpowers}
+""")
 
-ironman.networth = 20.5
-ironman.family_members = [50, "Natasha", "Nick Fury"]
+favourite_power = input("What is your fav superpower?\n")
+if favourite_power in superman.superpowers:
+    print("power already aquired")
+else:
+    superman.superpowers.add(favourite_power)
+print(superman.superpowers)
 
-haris = ironman
-haris.name = "haris"
-print(ironman.name)
-print(haris.name)
