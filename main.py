027687d4d8b4753c6ee0dd5dcb3b678b03ac0b02
dani <danielabraha99@gmail.com>
2021-09-21 12:19:53 +0100

    evaluate superpowers and make expenses

--- main.py
+++ main.py
@@ -35,14 +35,40 @@ Superman has the following powers:
 #     superman.superpowers.add(favourite_power)
 # print(superman.superpowers)
 
-power_check = input("Enter a superpower: ").lower()
-if superman.superpowers[power_check] >= 80:
-    print("SUPER COOL")
-elif superman.superpowers[power_check] > 50:
-    print("COOL")
-else:
-    print("YEAAAH AVERAGE")
+# power_check = input("Enter a superpower: ").lower()
+# if superman.superpowers[power_check] >= 80:
+#     print("SUPER COOL")
+# elif superman.superpowers[power_check] > 50:
+#     print("COOL")
+# else:
+#     print("YEAAAH AVERAGE")
 
 wallet = {"dollars": 170, "pounds": 50, "euros": 100}
 superman.wallet.update(wallet)
 print(superman.wallet)
+
+
+def evaluate_superpower(dict_of_sp, value):
+    list_of_sp = [(key, _value) for key, _value in dict_of_sp.items() if _value >= value]
+    # for key, _value in dict_of_sp.items():
+    #     if _value >= value:
+    #         list_of_sp.append((key, _value))
+    print(list_of_sp)
+
+
+def evaluate_money(wallet_dict, currency, num):
+    amount_spent = {}
+    for key, value in wallet_dict.items():
+        if currency == key and value >= num:
+            amount_spent[key] = num
+        elif currency == key and value < num:
+            amount_spent[key] = value
+        else:
+            amount_spent[currency] = "Currency not listed"
+    print(amount_spent)
+
+
+currency_input = input("specify the currency type: ")
+amount = int(input("How much do you want to spend: "))
+evaluate_money(wallet_dict=superman.wallet, currency=currency_input, num=amount)
+evaluate_superpower(superman.superpowers, 45)
