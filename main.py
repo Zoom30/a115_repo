aa95015141956a88e8ca5b3a6d8beadda3472e9d
dani <danielabraha99@gmail.com>
2021-09-21 10:23:15 +0100

    renamed character, add super power check and wallet

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ class Character:
         self.networth: float = 0
         self.family_members = []
         self.superpowers = None
+        self.wallet = {}
 
 
 superman = Character()
-superman.name = "Tony"
+superman.name = "Clark"
 superman.age = 36
 superman.date_of_birth = datetime.date(year=1986, month=3, day=15)
 # print(type(ironman.date_of_birth))
@@ -20,16 +21,28 @@ superman.date_of_birth = datetime.date(year=1986, month=3, day=15)
 age = (datetime.datetime.now().date() - superman.date_of_birth)
 superman.networth = 20.5
 superman.family_members = [50, "Natasha", "Nick Fury"]
-superman.superpowers = {"flying", "heat vision", "super strength", "super speed", "invincibility"}
+superman.superpowers = dict(
+    {"flying": 100, "heat vision": 75, "super strength": 45, "super speed": 89, "invincibility": 40})
 print(f"""
 Superman has the following powers:
 {superman.superpowers}
 """)
 
-favourite_power = input("What is your fav superpower?\n")
-if favourite_power in superman.superpowers:
-    print("power already aquired")
-else:
-    superman.superpowers.add(favourite_power)
-print(superman.superpowers)
+# favourite_power = input("What is your fav superpower?\n")
+# if favourite_power in superman.superpowers:
+#     print("power already acquired")
+# else:
+#     superman.superpowers.add(favourite_power)
+# print(superman.superpowers)
 
+power_check = input("Enter a superpower: ").lower()
+if superman.superpowers[power_check] >= 80:
+    print("SUPER COOL")
+elif superman.superpowers[power_check] > 50:
+    print("COOL")
+else:
+    print("YEAAAH AVERAGE")
+
+wallet = {"dollars": 170, "pounds": 50, "euros": 100}
+superman.wallet.update(wallet)
+print(superman.wallet)
