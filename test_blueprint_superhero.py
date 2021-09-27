a2e51ef1fc31f44836eb3b6ec3fec5ce7672adca
daniel <danielabraha99@gmail.com>
2021-09-27 09:54:26 +0100

    initial test using pytest commit

--- test_blueprint_superhero.py
+++ test_blueprint_superhero.py
@@ -1,5 +1,6 @@
 import unittest
 from blueprint_superhero import *
+import pytest
 
 
 class MyTestCase(unittest.TestCase):
@@ -17,27 +18,41 @@ class MyTestCase(unittest.TestCase):
         "Yen": 1000
     }
 
-    def test_evaluate_superpower_result(self):
-        self.assertEqual(evaluate_superpower(dict_of_sp=self.SAMPLE_DICTIONARY, value=85),
-                         second=[("super_speed", 90), ("flight", 99)])
-        self.assertEqual(evaluate_superpower(dict_of_sp={}, value=70), second=[])
-        self.assertEqual(evaluate_superpower(self.SAMPLE_DICTIONARY, value=100), second=[])
+    # def test_evaluate_superpower_result(self):
+    #     self.assertEqual(evaluate_superpower(dict_of_sp=self.SAMPLE_DICTIONARY, value=85),
+    #                      second=[("super_speed", 90), ("flight", 99)])
+    #     self.assertEqual(evaluate_superpower(dict_of_sp={}, value=70), second=[])
+    #     self.assertEqual(evaluate_superpower(self.SAMPLE_DICTIONARY, value=100), second=[])
+    #
+    # def test_evaluate_money_result(self):
+    #     self.assertEqual(evaluate_money(wallet_dict=self.SAMPLE_WALLET, currency="Dollars", num=20),
+    #                      second={"Dollars": 20})
+    #     self.assertEqual(evaluate_money(wallet_dict=self.SAMPLE_WALLET, currency="Yen", num=1500),
+    #                      second={"Yen": self.SAMPLE_WALLET["Yen"]})
+    #     self.assertEqual(evaluate_money(wallet_dict={}, currency="euros", num=50), second={})
+    #
+    # def test_evaluate_superpower_types(self):
+    #     self.assertRaises(AttributeError, evaluate_superpower, "text", 20)
+    #     self.assertRaises(TypeError, evaluate_superpower, self.SAMPLE_DICTIONARY, "fifty-two")
+    #
+    # def test_evaluate_money(self):
+    #     self.assertRaises(AttributeError, evaluate_money, "text", "Yen", 30)
+    #     self.assertRaises(TypeError, evaluate_money, self.SAMPLE_WALLET, 5465, "90")
 
-    def test_evaluate_money_result(self):
-        self.assertEqual(evaluate_money(wallet_dict=self.SAMPLE_WALLET, currency="Dollars", num=20),
-                         second={"Dollars": 20})
-        self.assertEqual(evaluate_money(wallet_dict=self.SAMPLE_WALLET, currency="Yen", num=1500),
-                         second={"Yen": self.SAMPLE_WALLET["Yen"]})
-        self.assertEqual(evaluate_money(wallet_dict={}, currency="euros", num=50), second={})
+    def test_evaluate_superpower_with_valid_dict_and_value(self):
+        assert (evaluate_superpower(dict_of_sp=self.SAMPLE_DICTIONARY, value=85)) == [("super_speed", 90),
+                                                                                      ("flight", 99)]
 
-    def test_evaluate_superpower_types(self):
-        self.assertRaises(AttributeError, evaluate_superpower, "text", 20)
-        self.assertRaises(TypeError, evaluate_superpower, self.SAMPLE_DICTIONARY, "fifty-two")
+    def test_evaluate_superpower_with_empty_dict_and_someValue(self):
+        assert (evaluate_superpower(dict_of_sp={}, value=50)) == []
 
-    def test_evaluate_money(self):
-        self.assertRaises(AttributeError, evaluate_money, "text", "Yen", 30)
-        self.assertRaises(TypeError, evaluate_money, self.SAMPLE_WALLET, 5465, "90")
+    # 'oor' -> 'Out of Range'
+    def test_evaluate_superpower_with_valid_dict_and_oor_value(self):
+        assert (evaluate_superpower(dict_of_sp=self.SAMPLE_DICTIONARY, value=100)) == []
+
+    def test_evaluate_money_with_valid_dict_and_num(self):
+        assert ()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
